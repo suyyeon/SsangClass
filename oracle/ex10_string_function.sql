6753e7328e299deb89673896ca91bb8710959f51
SUYEON <ch2oyeon@gmail.com>
2022-04-22 17:59:27 +0900

    0422

--- oracle/ex10_string_function.sql
+++ oracle/ex10_string_function.sql
@@ -88,6 +88,208 @@ from tblInsa;
 
 select * from tblInsa
     order by substr(ssn, 8, 1) asc;
+    
+
+
+
+
+
+/*
+
+    length()
+    - 문자열 길이
+    - number length(컬럼)
+
+*/    
+
+-- 컬럼 리스트에서 사용
+select name, length(name) from tblCountry;
+
+
+-- 조건절에 사용
+select name from tblCountry where length(name) > 3;
+select name from tblCountry where length(name) between 4 and 6;
+
+
+-- 정렬에 사용
+select name from tblCountry order by length(name) desc, name asc;
+
+
+-- 남자 > 여자 순
+select * from tblInsa order by substr(ssn, 8, 1) asc, ssn asc;
+
+
+-- 게시판 제목 > 길면 + 잘라서 + 말줄임표(..)
+select
+
+    title,
+    case
+        when length(title) >= 8 then substr(title, 1, 8) || '..'
+        else title
+    end 
+
+from tblTodo;
+
+
+
+/*
+
+    instr()
+    - 검색(= indexOf())
+    - 검색어의 위치를 반환
+    - number instr(컬럼, 검색어)
+    - number instr(컬럼, 검색어, 시작위치)
+    - 못찾으면 0을 반환
+
+*/
+
+select
+    
+    '안녕하세요. 홍길동님' as c1,
+    instr('안녕하세요. 홍길동님', '홍길동') as c2,
+    instr('안녕하세요. 홍길동님', '아무개') as c3,
+    instr('안녕하세요. 홍길동님. 반갑습니다. 홍길동님.', '홍길동') as c4,
+    instr('안녕하세요. 홍길동님. 반갑습니다. 홍길동님.', '홍길동') as c5,
+    instr('안녕하세요. 홍길동님. 반갑습니다. 홍길동님.', '홍길동', 11) as c6,
+    instr('안녕하세요. 홍길동님. 반갑습니다. 홍길동님.', '홍길동', instr('안녕하세요. 홍길동님. 반갑습니다. 홍길동님.', '홍길동') + length('홍길동')) as c7
+
+from dual;
+
+
+
+select * from tblInsa where tel like '010%';
+select * from tblInsa where substr(tel, 1, 3) = '010';
+select * from tblInsa where instr(tel, '010') = 1;
+
+
+
+
+
+/*
+
+    lpad(), rpad()
+    - left padding, right padding
+    - varchar2 lpad(컬럼, 개수, 문자)
+    - varchar2 rpad(컬럼, 개수, 문자)
+
+*/
+
+select
+    'a',
+    lpad('a', 5, 'b'),
+    '1',
+    lpad('1', 3, '0'),
+    rpad('1', 3, '0')
+from dual;
+
+
+/*
+
+    trim(), ltrim(), rtrim()
+    - varchar2 trim(컬럼)
+
+*/
+
+select
+    '      하나     둘      셋       ',
+    trim('      하나     둘      셋       '),
+    ltrim('      하나     둘      셋       '),
+    rtrim('      하나     둘      셋       ')
+from dual;
+
+
+
+/*
+
+    replace()
+    - 문자열 치환
+    - varchar2 replace(컬럼, 찾을 문자열, 바꿀 문자열)
+
+*/
+
+select
+    replace('홍길동', '홍', '김'),
+    replace('홍길동', '이', '김'),
+    replace('홍길홍', '홍', '김')
+from dual;
+
+
+
+select
+    name, ssn,
+    case
+        when substr(ssn, 8, 1) = '1' then '남자'
+        when substr(ssn, 8, 1) = '2' then '여자'
+    end as gender
+from tblInsa;
+
+
+select
+    name, ssn,
+    replace(replace(substr(ssn, 8, 1), '1', '남자'), '2', '여자') as gender
+from tblInsa;
+
+
+select
+    name,
+    continent,
+    case
+        when continent = 'AS' then '아시아'
+        when continent = 'EU' then '유럽'
+        when continent = 'AF' then '아프리카'
+    end as c,
+    replace(replace(replace(continent, 'AS', '아시아'), 'EU', '유럽'), 'AF', '아프리카'),
+    decode(continent, 'AS', '아시아', 'EU', '유럽', 'AF', '아프리카', 'NA', '북아메리카')
+from tblCountry;
+
+
+/*
+
+    decode()
+    - 문자열 치환
+    - varchar2 decode(컬럼, 찾을 문자열, 바꿀 문자열 [, 찾을 문자열, 바꿀 문자열] x N)
+    - 못찾으면 null 반환(***)
+    - case의 간단한 버전
+    
+*/
+
+select
+    substr(ssn, 8, 1),
+    replace(replace(substr(ssn, 8, 1), '1', '남자'), '2', '여자'),
+    decode(substr(ssn, 8, 1), '1', '남자', '2', '여자')
+from tblInsa;
+
+
+
+
+-- 남자? 여자?
+select
+
+    --A.
+    count(case
+        when gender = 'm' then 1
+    end),
+    count(case
+        when gender = 'f' then 1
+    end),
+    
+    --B.
+    count(decode(gender, 'm', 1)),
+    count(decode(gender, 'f', 1))
+    
+    
+from tblComedian;
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ oracle/ex13_ddl.sql
@@ -0,0 +1,293 @@
+-- ex13_ddl.sql
+
+/*
+
+    ex01 ~ ex12 : DML 기본
+    
+    DDL
+    - 데이터 정의어
+    - 데이터베이스 객체를 생성/수정/삭제한다.
+    - 데이터베이스 객체 > 테이블, 뷰, 인덱스, 프로시저, 트리거, 제약 사용, 시노닙 등..
+    - CREATE, ALTER, DROP
+    
+    테이블 생성하기 > 스키마 정의하기 > 속성(컬럼) 정의하기 > 속성(컬럼)의 성격(이름)과 도메인(자료형,제약)을 정의하기
+    
+    CREATE TABLE 테이블명
+    (
+        컬럼 정의,
+        컬럼 정의,
+        컬럼 정의,
+        컬럼 정의,
+        컬럼 정의,
+        
+        컬럼명 자료형(길이) NULL 제약사항
+        
+    );
+    
+    
+    
+    
+    제약 사항, Constraint
+    - 해당 컬럼에 들어갈 데이터(값)에 대한 조건
+        > 조건에 만족하면 > 대입
+        > 조건에 만족하지 못하면 > 에러발생
+    - 유효성 검사 도구
+    - 데이터 무결성을 보장하기 위한 도구(*****)
+    
+    
+    1. NOT NULL
+        - 해당 컬럼이 반드시 값을 가져야 한다.
+        - 해당 컬럼에 값이 없으면 에러 발생
+        - 필수값
+    
+    2. PRIMARY KEY, PK
+        - 기본키
+        - 테이블을 행을 구분하기 위한 제약 > 
+        - 행을 식별하는 수많은 키들 중 대표로 지정한 키
+        - 모든 테이블안에는 반드시 1개의 기본키가 존재해야 한다.(*******) 
+        - 중복값을 가질 수 없다. > unique
+        - 값을 반드시 가진다. > not null
+    
+    3. FOREIGN KEY
+    
+    4. UNIQUE
+        - 유일하다. > 행들간에서 동일값을 가질 수 없다.
+        - UNIQUE 제약은 null을 가질 수 있다. > 식별자가 될 수 있다.
+        ex) 경품
+            - 고객(번호(PK),이름,주소,당첨(UQ))
+                   1,홍길동,서울,1등
+                   2,아무개,부산,NULL
+                   3,하하하,서울,2등
+                   4,호호호,경기,3등
+                   5,후후후,제주,NULL
+        ex) 초등학교 교실
+            - 학생(번호(PK),이름,직책(UQ))
+                   1,홍길동,반장
+                   2,아무개,체육부장
+                   3,하하하,부반장
+                   4,호호호,NULL
+                   5,후후후,NULL
+    
+    5. CHECK
+        - 사용자 정의 제약 조건
+        - where절과 동일한 조건을 컬럼에 적용한다.
+        
+    
+    6. DEFAULT
+        - 기본값 설정
+        - insert/update 작업 때 컬럼값을 대입하지 않으면(null) null대신 미리 설정한 값을 넣는다.
+        
+
+*/
+
+
+drop table tblMemo;
+
+-- 메모 테이블
+create table tblMemo (
+
+    --컬럼명 자료형(길이) NULL 제약사항
+    seq number(3) null,                  --메모번호
+    name varchar2(30) null,              --작성자
+    memo varchar2(1000) null,            --메모
+    regdate date null                    --작성날짜
+
+);
+
+select * from tblMemo where name = '홍길동';
+
+insert into 테이블(컬럼리스트) values (값리스트);
+
+insert into tblMemo(seq, name, memo, regdate) values (1, '홍길동', '메모입니다.', sysdate);
+insert into tblMemo(seq, name, memo, regdate) values (2, '아무개', null, sysdate);
+insert into tblMemo(seq, name, memo, regdate) values (3, null, null, null);
+insert into tblMemo(seq, name, memo, regdate) values (null, null, null, null); -- 절대 생성 금지!!!!
+insert into tblMemo(seq, name, memo, regdate) values (4, '홍길동', '테스트', '2022-01-01'); -- 편법 
+insert into tblMemo(seq, name, memo, regdate) values (5, '홍길동', '테스트', '2022-01-01 12:30:00');
+
+insert into tblMemo(seq, name, memo, regdate) values (5, '홍길동', '테스트', to_date('2022-01-01 12:30:00', 'yyyy-mm-dd hh24:mi:ss')); -- FM
+
+
+
+
+drop table tblMemo;
+
+-- 메모 테이블
+create table tblMemo (
+
+    seq number(3) not null,              --메모번호(NN)
+    name varchar2(30) null,              --작성자
+    memo varchar2(1000) not null,        --메모(NN)
+    regdate date null                    --작성날짜
+
+);
+
+insert into tblMemo(seq, name, memo, regdate) values (1, '홍길동', '메모입니다.', sysdate);
+
+--ORA-01400: cannot insert NULL into ("HR"."TBLMEMO"."MEMO")
+insert into tblMemo(seq, name, memo, regdate) values (2, '홍길동', null, sysdate);
+insert into tblMemo(seq, name, memo, regdate) values (2, '홍길동', '', sysdate);  -- 빈문자('')도 null로 취급한다.
+insert into tblMemo(seq, name, memo, regdate) values (2, '홍길동', ' ', sysdate); 
+
+
+select * from tblMemo;
+
+
+
+
+
+
+drop table tblMemo;
+
+create table tblMemo (
+
+    seq number(3) primary key,           --메모번호(NN)
+    name varchar2(30) null,              --작성자
+    memo varchar2(1000) not null,        --메모(NN)
+    regdate date null                    --작성날짜
+
+);
+
+
+
+insert into tblMemo(seq, name, memo, regdate) values (1, '홍길동', '메모입니다.', sysdate);
+
+--ORA-00001: unique constraint (HR.SYS_C007081) violated > 중복값을 넣으려고 했습니다.
+insert into tblMemo(seq, name, memo, regdate) values (2, '홍길동', '메모입니다.', sysdate);
+
+--ORA-01400: cannot insert NULL into ("HR"."TBLMEMO"."SEQ")
+insert into tblMemo(seq, name, memo, regdate) values (null, '홍길동', '메모입니다.', sysdate);
+
+select * from tblMemo;
+
+
+
+
+
+
+
+
+
+
+drop table tblMemo;
+
+create table tblMemo (
+
+    seq number(3) primary key,           --메모번호(PK)
+    name varchar2(30) unique null,       --작성자(UQ)
+    memo varchar2(1000) not null,        --메모(NN)
+    regdate date null                    --작성날짜
+
+);
+
+insert into tblMemo(seq, name, memo, regdate) values (1, '홍길동', '메모입니다.', sysdate);
+
+--ORA-00001: unique constraint (HR.SYS_C007084) violated
+insert into tblMemo(seq, name, memo, regdate) values (2, '홍길동', '메모입니다.', sysdate);
+insert into tblMemo(seq, name, memo, regdate) values (2, '아무개', '메모입니다.', sysdate);
+
+insert into tblMemo(seq, name, memo, regdate) values (3, null, '메모입니다.', sysdate);
+insert into tblMemo(seq, name, memo, regdate) values (4, null, '메모입니다.', sysdate);
+
+
+select * from tblMemo;
+
+
+
+
+
+
+
+
+drop table tblMemo;
+
+create table tblMemo (
+
+    seq number(3) primary key,           --메모번호(PK)
+    name varchar2(30),                   --작성자
+    memo varchar2(1000),                 --메모
+    regdate date,                        --작성날짜
+    priority number check (priority between 1 and 3),                     --1(중요), 2(보통), 3(안중요)
+    category varchar2(30) check (category in ('할일','장보기','공부','가족','친구'))                --할일,장보기,공부,가족,친구
+
+);
+
+
+insert into tblMemo(seq, name, memo, regdate, priority, category) 
+                    values (1, '홍길동', '메모입니다.', sysdate, 1, '할일');
+
+-- ORA-02290: check constraint (HR.SYS_C007085) violated
+insert into tblMemo(seq, name, memo, regdate, priority, category) 
+                    values (2, '홍길동', '메모입니다.', sysdate, 5, '할일');     
+                    
+-- ORA-02290: check constraint (HR.SYS_C007086) violated
+insert into tblMemo(seq, name, memo, regdate, priority, category) 
+                    values (2, '홍길동', '메모입니다.', sysdate, 3, '코딩');   
+
+insert into tblMemo(seq, name, memo, regdate, priority, category) 
+                    values (2, '홍길동', '메모입니다.', sysdate, 3, null);  
+
+insert into tblMemo(seq, name, memo, regdate, priority, category) 
+                    values (3, '홍길동', '메모입니다.', sysdate, null, null);                     
+
+select * from tblMemo;
+
+
+
+
+
+
+
+
+
+
+
+drop table tblMemo;
+
+create table tblMemo (
+
+    seq number(3) primary key,           --메모번호(PK)
+    name varchar2(30) default '익명',    --작성자
+    memo varchar2(1000),                 --메모
+    regdate date default sysdate         --작성날짜
+   
+);
+
+insert into tblMemo(seq, name, memo, regdate) values (1, '홍길동', '메모입니다.', sysdate);
+insert into tblMemo(seq, name, memo, regdate) values (2, null, '메모입니다.', sysdate); --개발자의 의도 표현O
+
+-- 소극적인 default 실행 방법
+insert into tblMemo(seq, memo, regdate) values (3, '메모입니다.', sysdate); -- 개발자의 의도 표현X
+
+-- 적극적인 default 실행 방법
+insert into tblMemo(seq, name, memo, regdate) values (4, default, '메모입니다.', sysdate);
+
+select * from tblMemo;
+
+insert into tblMemo(seq, name, memo) values (1, '홍길동', '메모입니다.');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
