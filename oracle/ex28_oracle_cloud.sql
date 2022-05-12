f3c197d80cabb734d1d79b55d2513a43f118713a
SUYEON <ch2oyeon@gmail.com>
2022-05-12 23:06:26 +0900

    Update ex28_oracle_cloud.sql
    0512

--- oracle/ex28_oracle_cloud.sql
+++ oracle/ex28_oracle_cloud.sql
@@ -10,6 +10,136 @@
     - 무료 서비스 > 1인 사용 적당
     - 프로젝트 > N인 사용 부족..
     
+    
+    
+    가상 서버
+    - 운영체제 > Ubuntu 20.04 LTS
+    - 접속 계정 > "ubuntu" + ""
+    
+    
+    
+    pwd
+    - Print working directory
+    - 현재 작업중 폴더의 경로
+    ex) /home/ubuntu/ > ubuntu 계정의 홈디렉토리
+    
+    
+    ls <옵션> <디렉토리>
+    - List Direcotry Contents
+    - '-a' : all. 모든 파일과 디렉토리를 다 출력해라.(숨긴 파일/디렉토리)
+    - '-l' : long. 정보를 상세하게...
+    ex) ls -a -l
+    ex) ls -al
+    ex) ls -la
+    
+    
+    clear
+    - Clear Screen
+    
+    
+    방향키(위,아래)
+    - 여태 실행했던 명령어의 이력 탐색
+    
+    
+    man <명령어>
+    - User's Manual
+    
+    
+    cd
+    - change directory
+    ex) cd ..
+    ex) cd 자식디렉토리
+    ex) cd /
+    ex) cd ~ : 홈 디렉토리로 이동
+    ex) cd - : 이전 디렉토리로 이동
+    ex) cd : 홈 디렉토리로 이동
+    
+    
+    Prompt
+    > 계정명@서버이름:현재디렉토리$
+    
+    홈디렉토리(~)
+    > ubuntu@ubuntu:~$ 
+    
+    
+    ubuntu@ubuntu:/home$
+    
+    
+    
+    free
+    - 메모리 상태를 출력한다.
+    
+    
+    
+    date
+    - 현재 시각 출력
+    
+    
+    -----------------------------------------------------------------------------------
+    
+    쉘
+    1. bash
+    2. zsh
+    
+    
+    Swap
+    - 메모리의 부족을 보완하기 위해서 > 하드 디스크의 일부를 메모리처럼 사용하게 해준다.
+    
+    
+    루트 사용자 > root > 모든 권한을 가진 관리자
+    현재 로그인 계정 > ubuntu > 일반 사용자
+
+
+    현재 시각 설정하기
+    
+    1. $ sudo dpkg-reconfigure tzdata
+    2. Asia
+    3. Seoul
+    4. $ date
 
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
