b4d0a32ea359f3b812cc4f932159e3c59df0b4cf
SUYEON <ch2oyeon@gmail.com>
2022-05-15 23:31:20 +0900

    Update ex28_oracle_cloud.sql

--- oracle/ex28_oracle_cloud.sql
+++ oracle/ex28_oracle_cloud.sql
@@ -85,6 +85,24 @@
     Swap
     - 메모리의 부족을 보완하기 위해서 > 하드 디스크의 일부를 메모리처럼 사용하게 해준다.
     
+    1. $ sudo fallocate -l 4G /swapfile
+    2. $ sudo chmod 600 /swapfile
+    3. $ sudo mkswap /swapfile
+    4. $ sudo swapon /swapfile
+    5. $ free
+    6. 위의 설정의 서버 재시작할때도 유지
+        - /etc/fstab 파일
+        
+    
+    
+    파일 열기 > 보기 or 편집
+    1. cat <파일>
+        - 파일의 내용을 보여준다.
+        - 읽기
+        - $ cat /etc/fstab
+    
+    2. vi, vim, nano
+    
     
     루트 사용자 > root > 모든 권한을 가진 관리자
     현재 로그인 계정 > ubuntu > 일반 사용자
@@ -96,6 +114,46 @@
     2. Asia
     3. Seoul
     4. $ date
+
+    
+    
+    ----------------------------------------------------------------------------
+    
+    
+    리눅스에서 패키지(프로그램) 설치하기
+    - apt
+    - apt-get
+    
+    1. $ sudo apt-get update //현재 시스템의 패키지 상태를 최신 버전으로 업데이트
+    2. $ sudo apt-get install nginx
+    
+    3. $ sudo systemctl status nginx //현재 패키지의 상태를 보여준다. > active(running)
+    4. $ sudo systemctl stop nginx //실행중인 패키지를 중단한다. > inactive(dead)
+    5. $ sudo systemctl start nginx 
+    
+    6. $ sudo systemctl enable nginx  //서버 시작 시 같이 실행된다.
+    7. $ sudo systemctl disable nginx //자동 시작 중지
+    
+    
+    
+    오라클 클라우드에서 방화벽 열기
+    
+    우분투에서 방화벽 열기
+    - $ sudo iptables -I INPUT 1 -p tcp --dport 80 -j ACCEPT
+    - $ sudo iptables -I INPUT 1 -p tcp --dport 443 -j ACCEPT
+    
+    브라우저 주소창 > 129.154.216.218
+    
+    
+    
+    파일 호스트간에 전송 > FTP > SFTP
+    
+    Nginx 기본 홈 디렉토리 > /var/www/html
+
+    
+    권한 부여
+    - $ sudo chmod -R 777 /var/www/html
+    
 
 */
 
